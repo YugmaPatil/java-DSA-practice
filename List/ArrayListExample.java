f1e2841e48d540d052df944335288c0e85bdf875
YugmaPatil <yugmanemade@gmail.com>
2026-06-07 14:47:08 +0000

    "Update ArrayListExample code"

--- List/ArrayListExample.java
+++ List/ArrayListExample.java
@@ -1,1 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String> animals = new ArrayList<>();
+
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Horse");
+
+        System.out.println("ArrayList: " + animals);
+        System.out.println("First animal: " + animals.get(0));
+
+        animals.remove("Cat");
+        System.out.println("After removal: " + animals);
+        System.out.println("Size: " + animals.size());
+
+        Collections.sort(animals);
+        System.out.println("Sorted: " + animals);
+
+        Collections.reverse(animals);
+        System.out.println("Reversed: " + animals);
+    }
+}
