eba73216f53b5bb22970fd6dcd5cb436bcabccd9
YugmaPatil <yugmanemade@gmail.com>
2026-06-08 03:53:31 +0000

    Updated Linked List example

--- List/LinkedListExample.java
+++ List/LinkedListExample.java
@@ -1,1 +1,19 @@
+import java.util.*;
 
+public class LinkedListExample{
+    public static void main(String[] arg){
+        Stack<String> stack = new Stack<>();
+        stack.push("Java");
+        stack.push("Python");
+        stack.push("C and C++");
+        stack.push("R");
+
+          System.out.println("Stack:" + stack);
+
+        String removed= stack.remove(1);
+        System.out.println("Removed element : "+ removed);
+
+        System.out.println("Print remaining stack :" + stack);
+
+    }
+} 
