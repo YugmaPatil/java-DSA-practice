e29fa197683de341b90d52d47e055c53a37bdeed
YugmaPatil <yugmanemade@gmail.com>
2026-06-08 14:39:46 +0000

    TreeMapExample is added

--- Map/TreeMapExample.java
+++ Map/TreeMapExample.java
@@ -1,6 +1,7 @@
 import java.util.TreeMap;
 
-// TreeMap is a part of Java's Collections Framework and implements the Map interface. It stores key-value pairs in a sorted order based on the natural ordering of the keys or by a specified comparator. TreeMap does not allow null keys but allows null values. It provides log(n) time complexity for basic operations like get, put, and remove.
+// TreeMap is a part of Java's Collections Framework and implements the Map interface. It stores key-value pairs in a sorted order based on the 
+// natural ordering of the keys or by a specified comparator. TreeMap does not allow null keys but allows null values. It provides log(n) time complexity for basic operations like get, put, and remove.
 // In this example, we create a TreeMap that maps Integer keys to String values. We add some key-value pairs, print the TreeMap, remove a key, and print the TreeMap again to show the changes.
 // Note that the keys are automatically sorted in ascending order, which is a characteristic of TreeMap.
 // Output:
