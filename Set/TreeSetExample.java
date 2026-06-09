37c33fbbe9ea1731092d55e29811e2feb02f2e67
YugmaPatil <yugmanemade@gmail.com>
2026-06-09 03:59:24 +0000

    Performed basic operations on TreeSet and added to repository

--- Set/TreeSetExample.java
+++ Set/TreeSetExample.java
@@ -1,1 +1,31 @@
+import java.util.*;
 
+public class TreeSetExample{
+    public static void main(String[] args){
+
+        TreeSet<String> fine = new TreeSet<>();
+
+        fine.add("Imprisonment");
+        fine.add("Community Services");
+        fine.add("Pay Charges");
+        fine.add("Charges with Imprison");
+        fine.add("Pay Charges");
+
+        System.out.println("TreeSet :" +fine);
+        boolean remove = fine.remove("Imprisonment");
+
+        System.out.println("Print TreeSet :" + fine );
+
+       
+        System.out.println("Print first:" +fine.first());
+
+        System.out.println("Poll Last element:"+ fine.pollLast());
+
+        System.out.println("Now print the elements after operations performed :" + fine);
+        
+        fine.clear();
+        System.out.println("Current elements : "+  fine);
+
+
+    }
+}
